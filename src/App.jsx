5596336e5d203f9e4292cdc50afe373947561c16
Mateo Gago <patumateo@hotmail.com>
2026-04-30 01:05:35 -0300

    semantica

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,22 @@ import Skills from "./components/Skills";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 import ScrollToTop from "./components/ScrollToTop";
+import { Analytics } from "@vercel/analytics/next";
 function App() {
   return (
-    <div>
+    <>
       <Navbar />
-      <Hero />
-      <hr className="border-gray-200 max-w-4x1 mx-auto" />
-      <About />
-      <Skills />
-      <Projects />
-      <Contact />
+      <main>
+        <Hero />
+        <hr className="border-gray-200 max-w-4x1 mx-auto" />
+        <About />
+        <Skills />
+        <Projects />
+        <Contact />
+      </main>
       <ScrollToTop />
-    </div>
+      <Analytics />
+    </>
   );
 }
 
